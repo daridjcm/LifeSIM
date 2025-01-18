be1c4d8268a6b22d7a193383f436364424577dbb
daridjcm <daricorreamedina@gmail.com>
2025-01-18 16:39:28 -0500

    Initial commit
    Add dependences: React, React-DOM, TailwindCSS, PostCSS, Autoprefixer, Eslint and Vite.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/index.css'
+import './css/output.css';
+
+
+function App() {
+  return (
+    <>
+      <div id="app">
+        <h1 className='text-2xl'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
