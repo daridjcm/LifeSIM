45e63f0b5a5c4cddde39c63baea80e9f9b539974
daridjcm <daricorreamedina@gmail.com>
2025-01-19 16:05:28 -0500

    Links with redirection to index or login
    This is complement of Form.

---
+++ src/components/HaveOrNot.jsx
@@ -0,0 +1,11 @@
+export function Have() {
+  return (
+    <a className="text-xs m-auto underline cursor-pointer transition-all hover:text-blue-500" href="/login">Do you have an account?</a>
+  )
+}
+
+export function NotHave() {
+  return (
+    <a className="text-xs m-auto underline cursor-pointer transition-all hover:text-blue-500" href="/">Do you not have an account?</a>
+  )
+}
