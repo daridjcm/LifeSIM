11e4e9d8053560115beedd7318625a3c31d91852
daridjcm <daricorreamedina@gmail.com>
2025-01-24 13:14:00 -0500

    Remove path public/ to List

--- client/src/utils/List.js
+++ client/src/utils/List.js
@@ -1,10 +1,10 @@
 const list = [
-  { title: "Work", img: "public/images/work3d.jpeg", desc: "Go to" },
-  { title: "Bank", img: "public/images/bank3d.jpeg", desc: "Go to" },
-  { title: "Grocery", img: "public/images/supermarket3d.jpeg", desc: "Go to" },
-  { title: "Ocean Mall", img: "public/images/oceanmall3d.jpeg", desc: "Go to" },
-  { title: "Social", img: "public/images/social3d.jpeg", desc: "Go to" },
-  { title: "Home", img: "public/images/home3d.jpeg", desc: "Go to" },
-  { title: "Phone", img: "public/images/phone3d.jpeg", desc: "Go to" },
+  { title: "Work", img: "/images/work3d.jpeg", desc: "Go to" },
+  { title: "Bank", img: "/images/bank3d.jpeg", desc: "Go to" },
+  { title: "Grocery", img: "/images/supermarket3d.jpeg", desc: "Go to" },
+  { title: "Ocean Mall", img: "/images/oceanmall3d.jpeg", desc: "Go to" },
+  { title: "Social", img: "/images/social3d.jpeg", desc: "Go to" },
+  { title: "Home", img: "/images/home3d.jpeg", desc: "Go to" },
+  { title: "Phone", img: "/images/phone3d.jpeg", desc: "Go to" },
 ];
 export default list;
