f9e784dc8056ed5fe216cb015c41731afe33f75c
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-01-24 21:08:10 -0400

    Feat/init express backend (#13)
    * Add initial server setup with Express, .gitignore, package files, and sample endpoint for testing (#1).
    
    * chore: get object and save in var.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+app.use(bodyParser.json());
 
+const users = [];
+
+app.post("/addUser", (req, res) => {
+	const user = req.body;
+	users.push(user);
+
+	res.send({ users: users });
+});
+
+app.get("/getUsers", (req, res) => {
+	res.send({ users: users });
+});
 
 // una ruta de tipo GET para obtener cosas
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
