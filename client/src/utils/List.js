11b1750f09c6e527b0c16f74baa69b60889f80fa
daridjcm <daricorreamedina@gmail.com>
2025-02-05 18:36:08 -0500

    Changes filename of social3d to cafeteria3d

--- client/src/utils/List.js
+++ client/src/utils/List.js
@@ -3,8 +3,9 @@ const list = [
   { title: "Bank", img: "/images/bank3d.jpeg", desc: "Go to" },
   { title: "Grocery", img: "/images/supermarket3d.jpeg", desc: "Go to" },
   { title: "Mall", img: "/images/mall3d.jpeg", desc: "Go to" },
-  { title: "Cafeteria", img: "/images/social3d.jpeg", desc: "Go to" },
+  { title: "Cafeteria", img: "/images/cafeteria3d.jpeg", desc: "Go to" },
   { title: "Home", img: "/images/home3d.jpeg", desc: "Go to" },
   { title: "Phone", img: "/images/phone3d.jpeg", desc: "Go to" },
 ];
+
 export { list };
