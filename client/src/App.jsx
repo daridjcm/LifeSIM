192847d15ca1023e9fd223ce0a4f0aa94ee89012
daridjcm <daricorreamedina@gmail.com>
2025-02-07 20:43:22 -0500

    Fix conditional of Form to Login, SignUp and NewCustomer
    Finally, this is it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Game from "./pages/Game.jsx";
 import Login from "./pages/Login.jsx";
 import SignUp from "./pages/SignUp.jsx";
+import NewCustomer from "./pages/NewCustomer.jsx";
 
 function App() {
+  // TODO: Add page to routes that do not exist yet (404).
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Game />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
-        {/* <Route path="boss" element={<Boss />} /> */}
+        <Route path="customers/new" element={<NewCustomer />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Form/HaveOrNot.jsx
+++ client/src/components/Form/HaveOrNot.jsx
@@ -20,13 +20,15 @@ export function NotHave() {
   );
 }
 
-export function HaveNull() {
+export function HaveCustomers() {
   return (
     <a
       className="text-xs m-auto underline cursor-pointer transition-all hover:text-blue-500"
       href="/boss"
     >
-      Do you need help?
+      Do you need help? Call the boss
     </a>
   );
 }
+
+export default {HaveCustomers, Have, NotHave};
