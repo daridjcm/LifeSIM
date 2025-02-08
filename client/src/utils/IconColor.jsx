5fa1885d535390cb50cfb1e1660d876ac7d50283
daridjcm <daricorreamedina@gmail.com>
2025-02-08 14:03:26 -0500

    Changes name hungry to hunger and add new param Bladder

--- client/src/utils/IconColor.jsx
+++ client/src/utils/IconColor.jsx
@@ -1,22 +1,26 @@
-import { FireIcon, HeartIcon, Battery0Icon, Battery50Icon, Battery100Icon, UserMinusIcon } from "@heroicons/react/24/outline";
+import { FireIcon, HeartIcon, Battery0Icon, Battery50Icon, Battery100Icon, UserMinusIcon, FaceFrownIcon } from "@heroicons/react/24/outline";
 // get icon based on the need.name or name.value
 export default function getIconColor(name, value) {
-  switch (name) {
-    case "Hungry":
-      if (value <= 50) return <FireIcon className="w-6 h-6 text-yellow-500" />;
-      return <FireIcon className="w-6 h-6 text-red-500" />;
-    case "Health":
-      if (value <= 20) return <HeartIcon className="w-6 h-6 text-red-500" />;
-      if (value <= 50) return <HeartIcon className="w-6 h-6 text-yellow-500" />;
-      return <HeartIcon className="w-6 h-6 text-green-500" />;
-    case "Energy":
-      if (value <= 20) return <Battery0Icon className="w-6 h-6 text-red-500" />;
-      if (value <= 50) return <Battery50Icon className="w-6 h-6 text-yellow-500" />;
-      return <Battery100Icon className="w-6 h-6 text-yellow-500" />;
-    case "Hygiene":
-      if (value <= 20) return <UserMinusIcon className="w-6 h-6 text-amber-950" />;
-      return <UserMinusIcon className="w-6 h-6 text-blue-500" />;
-    default:
-      return null;
+  if (name === "Hunger") {
+    if (value <= 50) return <FireIcon className="w-6 h-6 text-yellow-500" />;
+    return <FireIcon className="w-6 h-6 text-red-500" />;
   }
-};
+  
+  if (name === "Health") {
+    if (value <= 20) return <HeartIcon className="w-6 h-6 text-red-500" />;
+    if (value <= 50) return <HeartIcon className="w-6 h-6 text-yellow-500" />;
+    return <HeartIcon className="w-6 h-6 text-green-500" />;
+  }
+
+  if (name === "Energy") {
+    if (value <= 20) return <Battery0Icon className="w-6 h-6 text-red-500" />;
+    if (value <= 50) return <Battery50Icon className="w-6 h-6 text-yellow-500" />;
+    return <Battery100Icon className="w-6 h-6 text-yellow-500" />;
+  }
+
+  if (name === "Hygiene") {
+    if (value <= 20) return <UserMinusIcon className="w-6 h-6 text-amber-950" />;
+    return <UserMinusIcon className="w-6 h-6 text-blue-500" />;
+  }
+  return <FaceFrownIcon className="w-6 h-6 text-purple-500" />;
+}
