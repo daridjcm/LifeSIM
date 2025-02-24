e6899a9feca42178b14cc964fb75ee22b92552d7
Daridjcm <daricorreamedina@gmail.com>
2025-02-24 12:51:04 -0500

    Add gender row to DB

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,8 @@ const { sequelize } = require('../config/database');
 const User = sequelize.define('User', {
   username: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false }
+  password: { type: DataTypes.STRING, allowNull: false },
+  gender: {type: DataTypes.STRING, allowNull: false}
 }, {
   timestamps: true,
   tableName: 'Users'
