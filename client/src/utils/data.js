5ff120aa60e9b3c621b76c0f35961659d11d7b2e
daridjcm <daricorreamedina@gmail.com>
2025-04-10 00:23:23 -0500

    Add diseases and treatments to each area

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,7 +1,9 @@
 import activities from "./ActivitiesUser.json";
 import productsData from "./Products.json";
 import doctorsData from "./Doctors.json"
+import diseasesData from "./Diseases.json"
 
 export const activitiesUser = activities;
 export const products = productsData;
 export const doctors = doctorsData;
+export const diseases = diseasesData;
