8467c8c072787d4849f81748b31d92bcb3f8ae0d
daridjcm <cdarianamedina@gmail.com>
2025-05-03 22:43:06 -0500

    Minor changes

--- client/src/components/ContentModal/Hospital/PillsReceted.jsx
+++ client/src/components/ContentModal/Hospital/PillsReceted.jsx
@@ -1,11 +1,30 @@
 import CustomButton from "../../CustomButton.jsx";
 import { Chip, ScrollShadow } from "@heroui/react";
+// import { useEffect, useState } from "react";
 
-// Render view to pills receted in the report
-// TODO: Get pills receted from API and display them here
+// const response = await fetch(
+//   "http://localhost:3000/api/appointments/report",
+//   {
+//     method: "GET",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(reportData),
+//   },
+// ); // Fetch the report data from the server
+
+// const pillsData = await response.JSON();
+
 export default function PillsReceted() {
+  // const [pills, setPills] = useState([]);
+
+  // useEffect(() => {
+  //   // Simulate fetching data
+  //   setPills(pillsData.treatments);
+  // }, [response]);
+  // TODO: Fix fetch, because causes error SIGILL
+
   return (
     <div className="flex sm:flex-col md:flex-row lg:flex-row justify-between w-full gap-5">
+      {/* Comercial Pills Section */}
       <div className="bg-zinc-100 sm:w-full md:w-[50%] lg:w-[50%] p-2">
         <p className="font-semibold">Pills Comercial</p>
         <div className="flex justify-between bg-zinc-200 p-4 rounded-lg">
@@ -32,6 +51,8 @@ export default function PillsReceted() {
           </ScrollShadow>
         </div>
       </div>
+
+      {/* Receted Pills Section */}
       <div className="bg-zinc-100 sm:w-full md:w-[50%] lg:w-[50%] p-2">
         <p className="font-semibold">Pills Receted</p>
         <div className="flex justify-between bg-zinc-200 p-4 rounded-lg">
@@ -39,22 +60,25 @@ export default function PillsReceted() {
             hideScrollBar
             className="sm:h-[550px] md:h-[400px] lg:h-[300px]"
           >
-            <div className="mb-2">
-              <p className="text-xl text-blue-500">Duloxetina</p>
-              <Chip
-                className="capitalize"
-                color="primary"
-                size="sm"
-                variant="flat"
-              >
-                30 tablets
-              </Chip>
-              <p className="text-sm">
-                It is used to treat depression in adults and generalized anxiety
-                disorder.
-              </p>
-            </div>
-            <CustomButton label="Collect it" variant="solid" size="sm" />
+            {pills.map((pill, index) => (
+              <div className="mb-4" key={index}>
+                <p className="text-xl text-blue-500">{pill.pill_name}</p>
+                <Chip
+                  className="capitalize"
+                  color="primary"
+                  size="sm"
+                  variant="flat"
+                >
+                  30 tablets
+                </Chip>
+                <p className="text-sm">{pill.pill_description}</p>
+                <CustomButton
+                  label={`Collect it ($${pill.pill_price})`}
+                  variant="solid"
+                  size="sm"
+                />
+              </div>
+            ))}
           </ScrollShadow>
         </div>
       </div>
