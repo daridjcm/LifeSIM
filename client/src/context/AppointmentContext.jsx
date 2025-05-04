4956d4fd2a601d333a57a1e95e8a31bdff316b9e
daridjcm <cdarianamedina@gmail.com>
2025-05-03 21:35:18 -0500

    Add new context to fetch appointments

---
+++ client/src/context/AppointmentContext.jsx
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const AppointmentContext = createContext();
+
+export const AppointmentProvider = ({ children }) => {
+  const [nextAppointment, setNextAppointment] = useState(null);
+
+  const fetchAppointments = async (userID) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/appointments");
+      if (!response.ok) throw new Error("Network response was not ok");
+  
+      const data = await response.json();
+      const userAppointments = data.appointments.filter(
+        (appt) => appt.user_id === userID,
+      );
+  
+      const now = new Date();
+      const upcoming = userAppointments
+        .map((appt) => ({
+          ...appt,
+          dateObj: new Date(`${appt.date}T${appt.time}Z`),
+        }))
+        .filter((appt) => new Date(appt.dateObj) > now)
+        .sort((a, b) => new Date(a.dateObj) - new Date(b.dateObj));
+  
+      setNextAppointment(upcoming[0] || null);
+  
+      return userAppointments;
+    } catch (error) {
+      console.error("Error fetching appointments:", error);
+      return [];
+    }
+  };
+  
+
+  return (
+    <AppointmentContext.Provider
+      value={{ nextAppointment, setNextAppointment, fetchAppointments }}
+    >
+      {children}
+    </AppointmentContext.Provider>
+  );
+};
+
+export const useAppointment = () => {
+  return useContext(AppointmentContext);
+};
